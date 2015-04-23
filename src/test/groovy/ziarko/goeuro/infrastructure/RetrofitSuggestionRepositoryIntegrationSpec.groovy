d439ba0b651eb2c20bb9c3a717ba88b13b4663df
Maciej Ziarko <maciej.ziarko@gmail.com>
2015-04-23 15:08:05 +0200

    Skipping tests using internet connection when internet is not available.

--- src/test/groovy/ziarko/goeuro/infrastructure/RetrofitSuggestionRepositoryIntegrationSpec.groovy
+++ src/test/groovy/ziarko/goeuro/infrastructure/RetrofitSuggestionRepositoryIntegrationSpec.groovy
@@ -3,11 +3,15 @@ package ziarko.goeuro.infrastructure
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.test.context.ContextConfiguration
+import spock.lang.Requires
 import spock.lang.Specification
 import spock.lang.Unroll
 import ziarko.goeuro.domain.SuggestionRepository
 
+import static RetrofitSuggestionRepositoryIntegrationSpec.isInternetAvailable
+
 @ContextConfiguration(loader = SpringApplicationContextLoader, classes = InfraConfig)
+@Requires({ isInternetAvailable() })
 class RetrofitSuggestionRepositoryIntegrationSpec extends Specification {
 
     @Autowired
@@ -26,6 +30,14 @@ class RetrofitSuggestionRepositoryIntegrationSpec extends Specification {
     def "Should access GoEuro remote service and correctly deserialize empty list response"() {
         expect:
             suggestionRepository.getSuggestionsForQuery('MustBeEmpty').isEmpty()
+    }
+
+    static boolean isInternetAvailable() {
+        try {
+            return !'http://google.com'.toURL().text.isEmpty()
+        } catch (Exception ex) {
+            return false
+        }
     }
 
 }
